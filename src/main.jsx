85e643dc0b34a00227b03b7dfb92023659228320
HgaMen <hga.int2021@gmail.com>
2022-11-25 16:17:11 -0600

    add react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import './index.css';
 
 import { Auth0Provider } from '@auth0/auth0-react';
+
+import { router } from './router';
+import { RouterProvider } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
@@ -12,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             clientId="AKWLOvTq9R9Wb5nt6fPadwmTTWW3SKcw"
             redirectUri={window.location.origin}
         >
-            <App />
+            <RouterProvider router={router} />
         </Auth0Provider>
     </React.StrictMode>
 );
