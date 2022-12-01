19061d048b8b81fffcf4cbd6b14d5a9c31f513a5
HgaMen <hga.int2021@gmail.com>
2022-12-01 16:10:40 -0600

    simple auth react router listo

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-import { Auth0Provider } from '@auth0/auth0-react';
-
 import { router } from './router';
 import { RouterProvider } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Auth0Provider
-            domain="dev-7h7y08q48tu1bjh6.us.auth0.com"
-            clientId="AKWLOvTq9R9Wb5nt6fPadwmTTWW3SKcw"
-            redirectUri={window.location.origin}
-        >
-            <RouterProvider router={router} />
-        </Auth0Provider>
+        <RouterProvider router={router} />
     </React.StrictMode>
 );
